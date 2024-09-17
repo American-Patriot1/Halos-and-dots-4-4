dc58fadf60d11e725137426084d22506c7f2e37b
Nathaniel Havens <97206671+American-Patriot1@users.noreply.github.com>
2024-09-17 20:55:04 +0000

    saving to test something

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-def distance_halo(image,targetX, targetY, modifier) :
+from PIL import Image
+texas=Image.open("Texas.png")
+def distance_halo(image,targetX, targetY, modifier):
     pixels=image.load()
     for x in range(image.width):
         for y in range(image.height):
-            last_guess= ((targetX**2)+(targetY**2))/2.0
-            while True:
+            X=0
+            Y=0
+            if x>=targetX:
+                X=x-targetX
+            else:
+                X=targetX-x
+            if y>=targetY:
+                Y=y-targetY
+            else:
+                Y=targetY-y
+            last_guess= ((X**2)+(Y**2))/2.0
+            i=False
+            distance=0
+            while i!=True:
                 guess=(last_guess + x/last_guess)/2
-                if abs(guess - last_guess) < .000001: # example threshold
-                    return guess
+                if abs(guess - last_guess) < .000001:
+                    distance=guess
+                    i=True
                 last_guess=guess
+            num=distance/modifier
+            r,g,b,t=pixels[x,y]
+            r=r-num
+            g=g-num
+            b=b-num
+            pixels[x,y]=r,g,b,t
+    image.save("halo.png")
+distance_halo(texas,512,342,2)
